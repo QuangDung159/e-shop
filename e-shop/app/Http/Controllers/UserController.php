c3b4f67b31c3ed15b1b94acd7de91d95bd52b61d
DungLQ159 <quangdunglu159@gmail.com>
2019-02-13 21:01:32 +0700

    - Add page to view user detail by role

--- e-shop/app/Http/Controllers/UserController.php
+++ e-shop/app/Http/Controllers/UserController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     // Route Page Methods
-    public function showDetailUserPage()
+    public function showDetailUserPage($user_id)
     {
-        return view("");
+        $user = User::find($user_id);
+        if ($user->role_id == 1) {
+            return view("");
+        } else {
+            return view("");
+        }
     }
     // End Page Route Methods
 
